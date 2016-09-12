5ccf995510292a0a8cf1d8e2f031bcebc812df19
CSS-Mechanic <luanshiyipiaoling@163.com>
2016-09-12 12:50:22 +0800

    导入配置

--- admin/app.js
+++ admin/app.js
@@ -3,3 +3,7 @@ require("bootstrap-webpack");
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import ngResource from 'angular-resource';
+import routesConfig from 'config/routes.config.js';
+
+angulat.modelue('Mall',[uiRouter,ngResource]);
+config(routesConfig);
