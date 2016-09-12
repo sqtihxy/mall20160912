95c8382a3b8cf0d51e7d41ac126e7fa08d4d1728
CSS-Mechanic <luanshiyipiaoling@163.com>
2016-09-12 12:30:05 +0800

    更新package.json文件并安装依赖,最后测试没错误,用npm start启动,地址是localhost:8080

--- admin/app.js
+++ admin/app.js
@@ -1,5 +1,5 @@
-require('jquery');
-require('angular');
-require('angular-ui-router');
-require('angular-resource');
-require('bootstrap-webpack');
+require('expose?$!expose?jQuery!jquery');
+require("bootstrap-webpack");
+import angular from 'angular';
+import uiRouter from 'angular-ui-router';
+import ngResource from 'angular-resource';
--- admin/webpack.config.js
+++ admin/webpack.config.js
@@ -1,13 +1,48 @@
 var webpack = require('webpack');
 var path = require('path');
-module.exports={
-    entry :'./app.js',
+
+module.exports = {
+    entry:['./app.js'],
     output:{
         path:path.resolve(__dirname,'dist'),
         filename:'bundle.js'
     },
-    resolve:{
-        root: ['./src'],
-        extensions:['','js','css']
+    module:{
+        loaders: [
+            {
+                test: /\.jade$/,
+                loader: 'jade',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                loader: "style!css" ,
+                exclude: /node_modules/
+            },
+            {
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                test: /\.js?$/,
+                query: {
+                    presets: ['es2015'],
+                },
+            },
+            {
+                test: /\.less$/,
+                exclude: /node_modules/,
+                loader: "style!css!less?strictMath&noIeCompat"
+            },
+            {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
+            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+        ]
+    },
+
+    resolve: {
+        root: [
+            path.resolve('./src') //指定根路径
+        ],
+        extensions:['','.js','.css']
     }
 }
